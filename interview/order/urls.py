e7204a7922ec84dc8220e526d4539b6ab7ea9630
Neil Kempin <nk@neils-mbp.mynetworksettings.com>
2024-06-02 22:58:48 -0400

    deactivate order feature
    PATCH requests to /order/<id>/active
    with a body providing {"is_active : true|false} will update
    order.is_active boolean feild for the provided id
    
    - create fixtures file for testing orders
    - add tests for deactivate order
    - add DeactivateOrderView to orders.views
    - add path to urls.py for deactivate order

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, DeactivateOrderView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('<int:id>/active', DeactivateOrderView.as_view(), name='activate-order'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +16,22 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def patch(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs['id'])
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        
+        serializer.save()
+
+        return Response(serializer.data, status=200)
+    
+    def get_queryset(self, **kwargs):
+        return self.queryset.get(**kwargs)
+        
