8538369fdae225a0c8ee5f42993b010e13a67e15
Neil Kempin <neilkempin@gmail.com>
2024-06-04 10:13:49 -0400

    add order-tags path to get all tags of an order
    - create OrderTagsRetrieveView
    - create tests

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderTagsRetrieveView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('<int:pk>/tags/', OrderTagsRetrieveView.as_view(), name="order-tags"),
     path('', OrderListCreateView.as_view(), name='order-list'),
 
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
-# Create your views here.
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderTagsRetrieveView(generics.RetrieveAPIView):
+
+    def get(self, request, **kwargs):
+        '''
+        Get all tags for Order
+        '''
+        tags = Order.objects.get(id=kwargs['pk']).tags.all()
+        serializer = OrderTagSerializer(tags, many=True)
+
+        return Response(serializer.data, status=200)
