8b2a7de02700aa86c7566dfffd3db1ac21050ee4
Neil Kempin <neilkempin@gmail.com>
2024-06-04 07:14:55 -0400

    add apps inventory, order to admin

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Inventory, InventoryType, InventoryTag, InventoryLanguage
 
-# Register your models here.
+# add inventory models to admin
+admin.site.register(Inventory)
+admin.site.register(InventoryType)
+admin.site.register(InventoryTag)
+admin.site.register(InventoryLanguage)
--- interview/order/admin.py
+++ interview/order/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Order, OrderTag
 
-# Register your models here.
+# add order models to admin
+admin.site.register(Order)
+admin.site.register(OrderTag)
